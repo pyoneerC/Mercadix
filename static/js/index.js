4eb54a37c002eb137f3bf870e61081b05a77c206
pyoneerC <maxcomperatutti@gmail.com>
2024-07-25 16:01:18 -0300

    fix spinner showing on back button

--- static/js/index.js
+++ static/js/index.js
@@ -5,3 +5,11 @@ document.getElementById('price-form').addEventListener('submit', function (event
       document.getElementById('price-form').submit();
    }, 100);
 });
+
+window.addEventListener('load', function() {
+   document.getElementById('spinner').style.display = 'none';
+});
+
+window.addEventListener('pageshow', function() {
+   document.getElementById('spinner').style.display = 'none';
+});
