3afcbd4d2b7af2d24174c4a36306e99253589dd3
pyoneerC <maxcomperatutti@gmail.com>
2025-04-04 19:13:19 -0300

    no counter

--- static/js/progressbar.js
+++ static/js/progressbar.js
@@ -1,25 +1,14 @@
-  document.getElementById('price-form').onsubmit = function(event) {
-    const numberOfPages = parseInt(document.getElementById('number_of_pages').value);
-    const totalSeconds = numberOfPages * 2;
+   document.getElementById('price-form').onsubmit = function (event) {
     const progressBar = document.getElementById('progress-bar');
-
-
     progressBar.style.width = '0%';
-    progressBar.textContent = totalSeconds + 's';
 
     let currentProgress = 0;
-    let secondsRemaining = totalSeconds;
-    const increment = 100 / totalSeconds;
-
     const interval = setInterval(() => {
-      if (currentProgress >= 100) {
-        clearInterval(interval);
-        progressBar.textContent = '0s';
-      } else {
-        currentProgress += increment;
-        progressBar.style.width = currentProgress + '%';
-        secondsRemaining -= 1;
-        progressBar.textContent = secondsRemaining + 's';
-      }
-    }, 1000);
-  }
+    if (currentProgress >= 100) {
+    clearInterval(interval);
+} else {
+    currentProgress += 2;
+    progressBar.style.width = currentProgress + '%';
+}
+}, 100);
+};
